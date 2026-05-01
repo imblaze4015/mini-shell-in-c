e6952766296f1ece3b1e3b635f19f762c9f3a73f
imblaze4015 <abdulmuhaymenali@gmail.com>
2026-05-02 00:36:29 +0800

    Add feature exit the program if the user press CTRL + D

--- mini-shell.c
+++ mini-shell.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (void){
-    //Buffer
-    char user_input [100];
+#define MAX_INPUT 1024
 
-    while (1) {
+int main (void){
+    //This is where the raw input of the user goes
+    char buffer [MAX_INPUT];
+    char *input;
+
+    while (1) 
+    {
         //Prompt
         printf(">> ");
 
-        //Catch user input and put in buffer
-        fgets(user_input, 100, stdin);
+        //Catch user input
+        input = fgets(buffer, MAX_INPUT, stdin);
+
+        //If user press CTRL + D then exit
+        if (input == NULL) 
+        {
+            printf("\n");
+            break;
+        }
 
         //Calculate the index of the "\n" and change it to NULL
-        user_input[strcspn(user_input, "\n")] = '\0';
+        input[strcspn(input, "\n")] = '\0';
 
-        if (strcmp(user_input, "exit") == 0)
+        //If user typed exit then exit the program
+        if (strcmp(input, "exit") == 0)
+        {
             break;
+        }
 
         //Print back what user typed
-        printf("%s\n", user_input);
+        printf("%s\n", input);
     }
 
 
